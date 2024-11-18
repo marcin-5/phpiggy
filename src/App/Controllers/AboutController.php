c8a1fbec80ba8ce85229253dffc680547301af33
Marcin Bojara <marcin_5@mail.ru>
2024-11-18 22:28:05 +0100

    Refactor AboutController constructor for dependency injection
    The AboutController class has been updated to use constructor property promotion, which simplifies initialization by directly injecting the TemplateEngine dependency. This change removes the unnecessary manual initialization, making the code cleaner and more concise.

--- src/App/Controllers/AboutController.php
+++ src/App/Controllers/AboutController.php
@@ -4,16 +4,12 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
-use App\Config\Paths;
 use Framework\TemplateEngine;
 
 class AboutController
 {
-    private TemplateEngine $view;
-
-    public function __construct()
+    public function __construct(private TemplateEngine $view)
     {
-        $this->view = new TemplateEngine(Paths::VIEW);
     }
 
     public function about(): void
