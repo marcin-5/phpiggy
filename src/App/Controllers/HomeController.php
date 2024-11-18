5a2daf2a3332d51e29cf8fcf6547ffbe7f2b2633
Marcin Bojara <marcin_5@mail.ru>
2024-11-18 22:23:06 +0100

    Refactor dependency injection container and HomeController
    Updated HomeController constructor for auto-injection and improved Container class with dependency resolution via reflection. Added detailed error handling for missing or unsupported type hints in Container.

--- src/App/Controllers/HomeController.php
+++ src/App/Controllers/HomeController.php
@@ -4,16 +4,12 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
-use App\Config\Paths;
 use Framework\TemplateEngine;
 
 class HomeController
 {
-    private TemplateEngine $view;
-
-    public function __construct()
+    public function __construct(private TemplateEngine $view)
     {
-        $this->view = new TemplateEngine(Paths::VIEW);
     }
 
     public function home(): void
--- src/Framework/Container.php
+++ src/Framework/Container.php
@@ -6,6 +6,7 @@ namespace Framework;
 
 use Framework\Exceptions\ContainerException;
 use ReflectionClass;
+use ReflectionNamedType;
 
 class Container
 {
@@ -16,13 +17,49 @@ class Container
         $this->definitions = [...$this->definitions, ...$newDefinitions];
     }
 
-    public function resolve(string $className)
+    public function resolve(string $className): object
     {
         $reflectionClass = new ReflectionClass($className);
         if (!$reflectionClass->isInstantiable()) {
             throw new ContainerException("Class {$className} is not instantiable");
         }
 
-        dd($reflectionClass);
+        $constructor = $reflectionClass->getConstructor();
+        if (!$constructor) {
+            return new $className;
+        }
+
+        $parameters = $constructor->getParameters();
+        if (count($parameters) === 0) {
+            return new $className;
+        }
+
+        $dependencies = [];
+        foreach ($parameters as $parameter) {
+            $name = $parameter->getName();
+            $type = $parameter->getType();
+            if (!$type) {
+                throw new ContainerException(
+                    "Failed to resolve class {$className} because of missing type hint for parameter {$name}"
+                );
+            }
+            if (!$type instanceof ReflectionNamedType || $type->isBuiltin()) {
+                throw new ContainerException(
+                    "Failed to resolve class {$className} because of unsupported type hint {$name}"
+                );
+            }
+            $dependencies[$name] = $this->get($type->getName());
+        }
+
+        return $reflectionClass->newInstanceArgs($dependencies);
+    }
+
+    public function get(string $id)
+    {
+        if (!array_key_exists($id, $this->definitions)) {
+            throw new ContainerException("Class {$id} does not exist in the container");
+        }
+        $factory = $this->definitions[$id];
+        return $factory();
     }
 }
