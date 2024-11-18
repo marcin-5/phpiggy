994b4ac002db1eaa3cf36038aa7daba7f6a79292
Marcin Bojara <marcin_5@mail.ru>
2024-11-18 17:52:42 +0100

    Add dependency injection container to App
    Introduced a new Container class and integrated it into the App class. This enhances dependency management and promotes better scalability within the framework.

--- src/Framework/App.php
+++ src/Framework/App.php
@@ -3,13 +3,16 @@
 declare(strict_types=1);
 
 namespace Framework;
+
 class App
 {
     private Router $router;
+    private Container $container;
 
     public function __construct()
     {
         $this->router = new Router();
+        $this->container = new Container();
     }
 
     public function run(): void
