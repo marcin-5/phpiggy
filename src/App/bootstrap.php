d19ebcad578a4b27738819688504aa0ebf268167
Marcin Bojara <marcin_5@mail.ru>
2024-11-18 17:42:59 +0100

    Add route registration function to centralize route definitions
    This commit introduces a new function `registerRoutes` in `src/App/Config/Routes.php` to handle route registration centrally. The `composer.json` file is updated to include this new configuration file. Additionally, `src/App/bootstrap.php` is modified to utilize `registerRoutes`, simplifying and decluttering the bootstrap file.

--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -4,11 +4,11 @@ declare(strict_types=1);
 
 require __DIR__ . '/../../vendor/autoload.php';
 
-use App\Controllers\{AboutController, HomeController};
 use Framework\App;
 
+use function App\Config\registerRoutes;
+
 $app = new App();
-$app->get('/', [HomeController::class, 'home']);
-$app->get('/about', [AboutController::class, 'about']);
+registerRoutes($app);
 
 return $app;
