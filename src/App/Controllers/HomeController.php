9f1ddc74360e719da6b86cae4fe81f598b5162ac
Marcin Bojara <marcin_5@mail.ru>
2024-11-18 15:00:34 +0100

    Integrate TemplateEngine for rendering the home page
    Refactor HomeController to use TemplateEngine for view rendering, enhancing maintainability. Added a new index.php view file to replace the hardcoded "Home page" string.

--- src/App/Controllers/HomeController.php
+++ src/App/Controllers/HomeController.php
@@ -4,10 +4,20 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
+use App\Config\Paths;
+use Framework\TemplateEngine;
+
 class HomeController
 {
+    private TemplateEngine $view;
+
+    public function __construct()
+    {
+        $this->view = new TemplateEngine(Paths::VIEW);
+    }
+
     public function home(): void
     {
-        echo 'Home page';
+        $this->view->render("/index.php");
     }
 }
