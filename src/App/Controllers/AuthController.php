a2716c81aff1d754ab17fad1011af5f5c2dd8e80
Marcin Bojara <marcin_5@mail.ru>
2024-11-21 00:19:38 +0100

    Add ValidatorService to AuthController
    Introduce ValidatorService for validation tasks in AuthController. Update container-definitions to include ValidatorService mapping. Create a new ValidatorService class.

--- src/App/Controllers/AuthController.php
+++ src/App/Controllers/AuthController.php
@@ -4,12 +4,15 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
+use App\Services\ValidatorService;
 use Framework\TemplateEngine;
 
 class AuthController
 {
-    public function __construct(private TemplateEngine $view)
-    {
+    public function __construct(
+        private TemplateEngine $view,
+        private ValidatorService $validator
+    ) {
     }
 
     public function registerView(): void
