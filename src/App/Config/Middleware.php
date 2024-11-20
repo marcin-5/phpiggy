b1f3b8969925fb36787011ea276bc20171b41c58
Marcin Bojara <marcin_5@mail.ru>
2024-11-20 12:38:07 +0100

    Add TemplateDataMiddleware to middleware registration
    Created a new TemplateDataMiddleware class and integrated it into the middleware configuration. This update ensures the middleware is properly registered and ready for use within the application.

--- src/App/Config/Middleware.php
+++ src/App/Config/Middleware.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace App\Config;
 
+use App\Middleware\TemplateDataMiddleware;
 use Framework\App;
 
 function registerMiddleware(App $app)
 {
+    $app->addMiddleware(TemplateDataMiddleware::class);
 }
