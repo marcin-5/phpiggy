750de43d633bff132c30961b7ee4ff939b923401
Marcin Bojara <marcin_5@mail.ru>
2024-11-20 12:29:23 +0100

    Add middleware registration to app bootstrap
    This commit introduces a new `Middleware.php` file in the `Config` directory and updates the `composer.json` to include this file. Additionally, a `registerMiddleware` function is added, and the `bootstrap.php` file is updated to register middleware during the app initialization process.

--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -7,9 +7,11 @@ require __DIR__ . '/../../vendor/autoload.php';
 use App\Config\Paths;
 use Framework\App;
 
+use function App\Config\registerMiddleware;
 use function App\Config\registerRoutes;
 
 $app = new App(Paths::SOURCE . "App/container-definitions.php");
 registerRoutes($app);
+registerMiddleware($app);
 
 return $app;
